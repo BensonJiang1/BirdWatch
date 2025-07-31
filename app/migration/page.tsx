415f29d852459a326682022730a98d2eb828dfe8
0H-Y3ah <136733009+0H-Y3ah@users.noreply.github.com>
2025-07-31 00:32:43 -0400

    fixed migration error

--- app/migration/page.tsx
+++
@@ -1,9 +0,0 @@
-import MigrationMap from '@/app/components/MigrationMap'; // Adjust the path based on your file structure
-
-export default function Migration() {
-  return (
-    <main>
-      <MigrationMap />
-    </main>
-  );
-}
